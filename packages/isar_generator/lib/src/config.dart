9c08451ae8c4adc576ee4018dea80a1481ed6328
Mohammed Almubarak <support1@posbank.me>
2024-04-03 14:23:46 +0300

    ..

--- packages/isar_generator/lib/src/config.dart
+++ packages/isar_generator/lib/src/config.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:yaml/yaml.dart';
 
 const _pubspecFile = 'pubspec.yaml';
-const _pubspecKey = 'objectbox';
+const _pubspecKey = 'isar';
 
 /// Config reads and holds configuration for the code generator.
 ///
@@ -17,39 +17,22 @@ const _pubspecKey = 'objectbox';
 ///   #   test: other
 /// ```
 class Config {
-
   Config({
-    String? jsonFile,
-    String? codeFile,
-    String? outDirLib,
-    String? outDirTest,
     this.classesToIgnore = const [],
     this.skipNotSupportedProperty = true,
-  })  : jsonFile = jsonFile ?? 'objectbox-model.json',
-        codeFile = codeFile ?? 'objectbox.g.dart',
-        outDirLib = outDirLib ?? '',
-        outDirTest = outDirTest ?? '';
+  });
 
   factory Config.readFromPubspec() {
     final file = File(_pubspecFile);
     if (file.existsSync()) {
       final yaml = loadYaml(file.readAsStringSync())[_pubspecKey] as YamlMap?;
       if (yaml != null) {
-        late final String? outDirLib;
-        late final String? outDirTest;
         late final bool skipNotSupportedProperty;
         late final List<String> classesToIgnore;
 
         final outDirYaml = yaml['output_dir'];
         final ignoreClasses = yaml['ignore_super_classes'];
         final optionsYaml = yaml['options'];
-
-        if (outDirYaml is YamlMap) {
-          outDirLib = outDirYaml['lib'] as String?;
-          outDirTest = outDirYaml['test']as String?;
-        } else {
-          outDirLib = outDirTest = outDirYaml as String?;
-        }
 
         if (ignoreClasses is YamlList) {
           classesToIgnore = ignoreClasses.nodes.map((e) => e.value.toString()).toList();
@@ -64,8 +47,6 @@ class Config {
         }
 
         return Config(
-          outDirLib: outDirLib,
-          outDirTest: outDirTest,
           classesToIgnore: classesToIgnore,
           skipNotSupportedProperty: skipNotSupportedProperty,
         );
@@ -73,10 +54,6 @@ class Config {
     }
     return Config();
   }
-  final String jsonFile;
-  final String codeFile;
-  final String outDirLib;
-  final String outDirTest;
   final List<String> classesToIgnore;
   final bool skipNotSupportedProperty;
 }
