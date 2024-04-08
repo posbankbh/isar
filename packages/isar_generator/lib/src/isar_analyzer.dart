fc791c345d0bd89d82cb347a6547dcb255255da1
Mohammed Almubarak <support1@posbank.me>
2024-04-08 03:34:25 +0300

    ..

--- packages/isar_generator/lib/src/isar_analyzer.dart
+++ packages/isar_generator/lib/src/isar_analyzer.dart
@@ -495,14 +495,11 @@ class IsarAnalyzer {
   String? _checkConverters(DartType fieldDartType, ClassElement classElement) {
     final converters = classElement.collectionAnnotation?.converters ?? classElement.embeddedAnnotation?.converters;
 
-    err(converters?.length.toString() ?? 'null');
-
     if (converters != null) {
       for (final converter in converters) {
-        final mirror = reflectType(converter.runtimeType, [fieldDartType.runtimeType]);
-        err(converter.runtimeType.toString());
+        final mirror = reflectType(converter, [fieldDartType.runtimeType]);
         if (mirror.hasReflectedType) {
-          return MirrorSystem.getName(mirror.simpleName);
+          err(mirror.owner.toString());
         }
       }
 
